366da692e930b4a68f91abb34f77377064254664
Andrew Reasoa <mail@andrewreasoa.com>
2016-05-21 22:13:09 +0200

    Added gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@
  */
 
 // declare vars
-var gulp, sass, sourcemaps, config;
+var gulp, sass, sourcemaps, prefix, config;
 
 // init plugins
 gulp = require('gulp');
 sass = require('gulp-sass');
 sourcemaps = require('gulp-sourcemaps');
+prefix = require('gulp-autoprefixer');
