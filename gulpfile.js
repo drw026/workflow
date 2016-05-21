225569959bb8b593620d9e986a1c5ead41b6c2bd
Andrew Reasoa <mail@andrewreasoa.com>
2016-05-21 23:31:28 +0200

    Added concat

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,57 @@
  */
 
 // declare vars
-var gulp, sass, sourcemaps, prefix, config;
+var gulp, sass, sourcemaps, prefix, concat, config;
 
 // init plugins
 gulp = require('gulp');
 sass = require('gulp-sass');
 sourcemaps = require('gulp-sourcemaps');
 prefix = require('gulp-autoprefixer');
+concat = require('gulp-concat');
+
+// config
+config = {
+
+    paths: {
+
+        app: {
+            scss_src: 'app/scss/**/*.scss',
+            css_dest: 'app/css'
+        },
+
+        dist: {
+            // dist paths
+        }
+
+    }
+
+    browserSupport: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+}
+
+// default gulp task
+gulp.task('default' function() {
+
+    // watch tasks
+
+});
+
+gulp.task('sass', function() {
+
+    return gulp.src(config.paths.app.scss_src)
+
+        .pipe(sourcemaps.init())
+
+        .pipe(sass({
+            errLogToConsole: true,
+            includePaths: [
+                'app/scss/'
+            ]
+        }))
+
+        .pipe(autoprefixer({
+            browsers: autoPrefixBrowserList,
+            cascade:  true
+        }))
+
+});
